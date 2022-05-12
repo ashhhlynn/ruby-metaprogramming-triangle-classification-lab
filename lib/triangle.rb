a3334ccd88c127f709bed33bb3b2a5ee9ba72f44
ashhhlynn <ashleylynnwelch@gmail.com>
2022-05-11 21:16:39 -0500

    commit tree f75f08f0c480d3f97a9253e10bb7d89458b122a2

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,31 @@
 class Triangle
-  # write code here
-end
+  attr_accessor :sideone, :sidetwo, :sidethree
+
+  def initialize(sideone, sidetwo, sidethree)
+    @sideone = sideone
+    @sidetwo = sidetwo 
+    @sidethree = sidethree
+  end 
+
+def kind 
+if @sideone <= 0 || @sidetwo <= 0 || @sidethree <= 0
+  raise TriangleError
+elsif @sideone + @sidetwo <= @sidethree || @sidetwo + @sidethree <= @sideone || @sideone + @sidethree <= @sidetwo
+ raise TriangleError
+elsif 
+@sideone == @sidetwo && @sideone == @sidethree
+puts :equilateral
+elsif 
+@sideone != @sidetwo && @sideone != @sidethree 
+puts :scalene 
+else 
+puts :isoscles 
+end 
+end 
+
+
+class TriangleError < StandardError
+def message
+end 
+
+end 
